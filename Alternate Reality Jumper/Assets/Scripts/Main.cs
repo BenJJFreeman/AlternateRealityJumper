ed3e555cf6c32ca027fe47ec79a05e954f71045e
Ben <benjjfreeman@gmail.com>
2020-01-09 20:59:25 +0000

    Initial Project

---
+++ Alternate Reality Jumper/Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public TaskManager taskManager;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
